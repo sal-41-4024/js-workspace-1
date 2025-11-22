a562262a4d06903391c9ba6e43004e4945c9c608
sal-41-4024 <log28002820@mail.ru>
2025-11-22 00:24:20 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const characters = [
-  {name: 'мечник', health: 10},
+  {name: 'мечник', health: 100},
   {name: 'копейщик', health: 100},
-  {name: 'копейщик', health: 0},
-  {name: 'лучник', health: 0},
+  {name: 'копейщик', health: 100},
+  {name: 'лучник', health: 100},
 ];
 
 const alive = characters.filter(item => item.health > 0);
